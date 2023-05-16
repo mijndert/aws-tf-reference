2370035b039662aaef112d639874e4d816cb4106
Mijndert Stuij <mijndert@mijndertstuij.nl>
2023-05-16 11:29:18 +0200

    Fixes typo

--- environments/example/01-account/state.tf
+++ environments/example/01-account/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     dynamodb_table = "terraform-locks"
-    bucket         = "terraform-state-mijndert"
+    bucket         = "terraform-state-example"
     encrypt        = true
     key            = "01-account.tfstate"
     region         = "eu-west-1"
--- environments/example/02-vpc/state.tf
+++ environments/example/02-vpc/state.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     dynamodb_table = "terraform-locks"
-    bucket         = "terraform-state-mijndert"
+    bucket         = "terraform-state-example"
     encrypt        = true
     key            = "02-vpc.tfstate"
     region         = "eu-west-1"
